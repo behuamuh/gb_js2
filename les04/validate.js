827bb99acdb2c29b9851d4dcef80308ba2062b37
Ben <sven1980@list.ru>
2018-08-20 06:05:59 +0300

    дуы4ду

---
+++ les04/validate.js
@@ -0,0 +1,44 @@
+var sities = [];
+$(function () {
+    $.getJSON('http://89.108.65.123/cities',function(data){
+                    $.each(data, function(key, val){
+                        sities.push(val['name']);
+        }
+    )});
+    //console.log(sities);
+    $('form').submit(function () {
+        var namePtrn = '^[a-zа-яё ]{1,}$';
+        var phonePtrn = '^\\+7\\(\\d{3}\\)\\d{3}\\-?\\d{4}';
+        var emailPtrn = '^\\w+@\\w+\\.[a-z]+$';
+        var txtPtrn = '.';
+        var valid = checkElement($('#name'), namePtrn);
+        valid = checkElement($('#phone'), phonePtrn) && valid;
+        valid = checkElement($('#email'), emailPtrn) && valid;
+        valid = checkElement($('#textarea'), txtPtrn) && valid;
+        return valid;
+    });
+    $('#selectCity').keyup(function(){
+        var inpStr = $(this).val();
+        if(inpStr.length > 2){
+            sities.forEach(function(elem){
+                if(elem.toLowerCase().indexOf(inpStr.toLowerCase()) == 0){
+                    console.log(inpStr);
+                    $('#dl').append('<option value="'+ elem +'">');
+                }
+            })
+        }
+    })
+});
+
+function checkElement(elem, ptrn) {
+    var str = elem.val();
+    if (new RegExp(ptrn, 'igm').test(str)) {
+        elem.css('border', '');
+        return true;
+    }
+    else {
+        elem.css('border', '1px solid red');
+        //console.log('error');
+        return false;
+    }
+}
