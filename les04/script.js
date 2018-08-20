b408bb08e2fd772c09636a7143bd2b6bc8b39143
behuamuh <sven1980@list.ru>
2018-08-20 13:06:44 +0300

    Update script.js

--- les04/script.js
+++ les04/script.js
@@ -4,12 +4,17 @@ $(function () {
     btns.each(function (ind, item) {
         $(item).on('click', function () {
             show(ind + 1);
+            $(this).addClass('btn-ligth').removeClass('btn-dark');
         })
-    })
+    });
+    $(btns.eq(0)).click();
 });
 function hideAll() {
     $('.mytabs').each(function (i, elem) {
         $(elem).hide();
+    })
+    $('button').each(function(){
+    		$(this).addClass('btn-dark').removeClass('btn-ligth');
     })
 }
 function show(id) {
